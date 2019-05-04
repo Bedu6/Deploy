5c59f27cf9af5b1e0257298df165e5cbcf4df595
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2019-05-04 14:17:38 -0500

    heroku

--- server/index.js
+++ server/index.js
@@ -13,4 +13,12 @@ require('./routes/gatosRoutes')(app);
 // const gatosRoutes = require('./routes/gatosRoutes');
 // gatosRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+	});
+}
+
 app.listen(process.env.PORT || 5000);
